c6c6b9b9a377fc8c6033e6cb450d0204d432d8db
Sebastian Riedel <git@basxto.de>
2020-05-05 21:22:56 +0200

    Compress concatenated images in one go

--- png2gb.py
+++ png2gb.py
@@ -22,6 +22,60 @@ def convert_tile(x, y, pixel):
                 lobyte=0
     return tile
 
+def data2array(data):
+    # clean up
+    data = data.replace("\n","").replace("\t","")[:-2]
+    # convert to array
+    arr = data.split(", ")
+    return arr
+
+def array2data(arr):
+    formatted_output = ""
+    counter = 0
+    for a in arr:
+        if counter == 0:
+            formatted_output += "\n\t"
+        formatted_output += "{0}, ".format(a)
+        counter = (counter + 1) % 16
+    return formatted_output[1:] + "\n"
+
+# function for popping last character from encoding
+def enc_pop(encoding_pair):
+    element = 0
+    return element, encoding_pair
+
+# we don't have to look at the end of verbatim,
+# that's the only thing that got already done
+# verbatim blocks are still what we want to improve
+
+# works with those, having onesteps (80, 70, C0) followed by verbatim:
+#####
+
+# 10 could be followed by two increments of it, same goes for C0
+# -> saves nothing, slows down skipping
+# -> unless that’s all the verbatim contains = 1 byte saved
+
+# 07 could be followed by repetitions of the last number
+# -> could become C0 or 80
+
+# 10 and 07 could be followed by alternations
+# -> could become C0 or A0 or E0
+
+# works with those who could be transformed to a shorter notation with one or two additional numbers
+#####
+
+# 3 byte notations to 1 or 2 byte notations
+# 2 byte notations to 1 byte notation
+
+# verbatim that can change encoding with one or two additional numbers:
+#####
+
+# this is basically a special case of the first section
+# can this even save space?
+
+def improve_compression(output):
+    return output
+
 # helper function for compress_rle
 def flush_verbatim(verbatim, output, datasize):
     if(args.color_line_compression == "yes" and len(verbatim) == 2 and (verbatim[0] == '0xFF' or verbatim[0] == '0x00') and (verbatim[1] == '0xFF' or verbatim[1] == '0x00')):
@@ -32,24 +86,30 @@ def flush_verbatim(verbatim, output, datasize):
             h = 0x10
         if verbatim[1] == '0xFF':
             l = 0x08
-        output.append("(0x{0:02X})".format(0xC0 | h | l | 0))
+        output.append(["(0x{0:02X})".format(ENC_LIN | h | l | 0),[]])
         datasize += 1
     else:
-        output.append("(0x{0:02X})".format(len(verbatim)-1))
-        output += verbatim
+        output.append(["(0x{0:02X})".format(ENC_LIT | len(verbatim)-1), verbatim.copy()])
         datasize += 1+len(verbatim)
     return datasize, output
+
+ENC_LIT = 0x00
+ENC_INC = 0x70
+ENC_RUN = 0x80
+ENC_INV = 0xA0
+ENC_LIN = 0xC0
+ENC_ALT = 0xE0
 
 # current:
 # 2 bytes don’t have to be repeated more than 16 times (2 tiles)
 # one byte doesn’t have to run more than 32 times (2 tiles)
-# 00 0XXXXXXX - write through the next X bytes (127+1-15)
-# 70 0111XXXX - run next byte X times incrementing it each time (15+2) - map compression
-# 80 100XXXXX - run next byte X times (31+2) - highest and lowest color only
-# A0 101XXXXX - run next byte X*2 times alternating normal and inverted  (31+2) - middle colors only
-# C0 110HLXXX - High Low colored line X times (7+1)
-# E0 111XXXXX - run next 2 bytes X times alternating (30+2)
-# FF 11111111 - end of data
+# 00 0XXXXXXX - write through the next X bytes (127+1-15) [1 + n bytes]
+# 70 0111XXXX - run next byte X times incrementing it each time (15+2) - map compression [2 bytes]
+# 80 100XXXXX - run next byte X times (31+2) - highest and lowest color only [2 bytes]
+# A0 101XXXXX - run next byte X*2 times alternating normal and inverted  (31+2) - middle colors only [2 bytes]
+# C0 110HLXXX - High Low colored line X times (7+1) [1 byte]
+# E0 111XXXXX - run next 2 bytes X times alternating (30+2) [3 bytes]
+# FF 11111111 - end of data [1 byte]
 
 # 1101 1111 represents 8 times 0xFF 0xFF, which are 16 bytes / one tile
 
@@ -58,14 +118,11 @@ def flush_verbatim(verbatim, output, datasize):
 def compress_rle(data):
     # we calculate new compressed datasize
     datasize = 0
-    # clean up
-    data = data.replace("\n","").replace("\t","")[:-2]
-    # convert to array
-    data = data.split(", ")
+    data = data2array(data)
     output = []
     verbatim = [data[0]]
     # 0 is one byte, 1 is 2 bytes and 2 is incremental
-    mode = 0 
+    mode = 0
     # first character has run of 1
     counter = 1
     # first character is already in verbatim buffer
@@ -91,8 +148,7 @@ def compress_rle(data):
                     counter -= 1
                     append = True
                 if(counter > 1):
-                    output.append("( 0x{0:02X} )".format(0x80 | (counter-2)))
-                    output.append(data[i])
+                    output.append(["( 0x{0:02X} )".format(ENC_RUN | (counter-2)),[data[i]]])
                     datasize += 2
                 if append:
                     verbatim.append(data[i])
@@ -122,17 +178,14 @@ def compress_rle(data):
                             h = 0x10
                         if data[i-1] == '0xFF':
                             l = 0x08
-                        output.append("(0x{0:02X})".format(0xC0 | h | l | (int(counter)-1)))
+                        output.append(["(0x{0:02X})".format(ENC_LIN | h | l | (int(counter)-1)),[]])
                         datasize += 1
                     # only invert on a byte scale
                     elif int(data[i-2],16) == (int(data[i-1],16)^0xFF):
-                        output.append("((0x{0:02X}))".format(0xA0 | (counter-2)))
-                        output.append(data[i-2])
+                        output.append(["((0x{0:02X}))".format(ENC_INV | (counter-2)),[data[i-2]]])
                         datasize += 2
                     else:
-                        output.append("((0x{0:02X}))".format(0xE0 | (counter-2)))
-                        output.append(data[i-2])
-                        output.append(data[i-1])
+                        output.append(["((0x{0:02X}))".format(ENC_ALT | (counter-2)), [data[i-2], data[i-1]]])
                         datasize += 3
                 if append:
                     verbatim.append(data[i])
@@ -153,8 +206,7 @@ def compress_rle(data):
                     counter -= 1
                     inc += 1
                     append = True
-                output.append("(( 0x{0:02X}))".format(0x70 |  (counter-2)))
-                output.append("0x{0:02X}".format(inc))
+                output.append(["(( 0x{0:02X}))".format(0x70 |  (counter-2)), [inc]])
                 datasize += 2
                 if append:
                     verbatim.append(data[i])
@@ -167,36 +219,30 @@ def compress_rle(data):
                     hl = 0x0
                     if data[i-1] == '0xFF':
                         hl = 0x18
-                    output.append("(0x{0:02X})".format(0xC0 | hl | (int(counter/2)-1)))
+                    output.append(["(0x{0:02X})".format(ENC_LIN | hl | (int(counter/2)-1)), []])
                     datasize += 1
                 else:
-                    output.append("(0x{0:02X})".format(0x80 | (counter-2)))
-                    output.append(data[i-1])
+                    output.append(["(0x{0:02X})".format(ENC_RUN | (counter-2)),[data[i-1]]])
                     datasize += 2
             elif mode == 1:
                 if args.color_line_compression == "yes" and  counter <= 8 and (data[i-2] == '0xFF' or data[i-2] == '0x00') and (data[i-1] == '0xFF' or data[i-1] == '0x00'):
-                    #print("We could optimize this")
                     h = 0x0
                     l = 0x0
                     if data[i-2] == '0xFF':
                         h = 0x10
                     if data[i-1] == '0xFF':
                         l = 0x08
-                    output.append("(0x{0:02X})".format(0xC0 | h | l | (int(counter)-1)))
+                    output.append(["(0x{0:02X})".format(ENC_LIN | h | l | (int(counter)-1)),[]])
                     datasize += 1
                 elif int(data[i-2],16) == (int(data[i-1],16)^0xFF):
-                    output.append("(0x{0:02X})".format(0xA0 | (counter-2)))
-                    output.append(data[i-2])
+                    output.append(["(0x{0:02X})".format(ENC_INV | (counter-2)),[data[i-2]]])
                     datasize += 2
                 else:
-                    output.append("(0x{0:02X})".format(0xE0 | (counter-2)))
-                    output.append(data[i-2])
-                    output.append(data[i-1])
+                    output.append(["(0x{0:02X})".format(ENC_ALT | (counter-2)),[data[i-2], data[i-1]]])
                     datasize += 3
             else: # mode 2
-                output.append("(0x{0:02X})".format(0x70 |  (counter-2)))
                 # we have to calculate this once 0x01 would have 0x01 as data
-                output.append("0x{0:02X}".format(int(data[i-1], 16) - counter + 1))
+                output.append(["(0x{0:02X})".format(0x70 |  (counter-2)), ["0x{0:02X}".format(int(data[i-1], 16) - counter + 1)]])
                 datasize += 2
             verbatim.append(data[i])
             counter = 1
@@ -233,18 +279,22 @@ def compress_rle(data):
             verbatim.append(data[i])
             counter = 1
         i+=1
+    # save even more bytes
+    # from decompnessor view it’s free
+    output = improve_compression(output)
+    # flatten output
+    #print(output)
+    flatoutput = []
+    for o in output:
+        flatoutput.append(o[0])
+        flatoutput+=o[1]
+    #print(flatoutput)
+    if datasize != len(flatoutput):
+        print("Warning: counting is wrong {} vs {}".format(datasize, len(flatoutput)))
     # last byte
     if len(verbatim) > 0:
         datasize, output = flush_verbatim(verbatim, output, datasize)
-    # format output
-    formatted_output = ""
-    counter = 0
-    for o in output:
-        if counter == 0:
-            formatted_output += "\n\t"
-        formatted_output += "{0}, ".format(o)
-        counter = (counter + 1) % 16
-    return datasize, formatted_output[1:] + "\n"
+    return datasize, array2data(flatoutput)
 
 def convert_image(width, height, filebase, pixel, d, m):
     global mapper
@@ -266,20 +316,28 @@ def convert_image(width, height, filebase, pixel, d, m):
                     if compress and (tile in mapper):
                         # use existing tile
                         if(mapcounter < args.limit):
-                            dmap += "0x{0:02X}, ".format(mapper[tile])
+                            dmap += "0x{0:02X}, ".format(mapper[tile][0])
+                            mapper[tile][1] += 1 # count occurences
                     else:
                         # add new tile
                         if(mapcounter < args.limit):
                             data += "\t" + tile + "\n"
                             datasize += 16
-                        mapper[tile] = mapcounter
+                        mapper[tile] = [mapcounter, 1]
                         if(mapcounter < args.limit):
                             dmap += "0x{0:02X}, ".format(mapcounter)
                         mapcounter += 1
-                    
                     mapsize += 1
                     maprealsize += 1
     return data, dmap
+
+# We aim for ENC_LIN optimization foremost
+# 2 byte encoding after that
+def mapping_optimizer(data, dmap):
+    sortedkeys = list({k: v for k, v in sorted(mapper.items(), key=lambda item: item[1][1], reverse=True)})
+    #print("Most used mapping is 0x{0:02X} ({1} times)".format(mapper[sortedkeys[0]][0], mapper[sortedkeys[0]][1]))
+    #print("Second most used mapping is 0x{0:02X} ({1} times)".format(mapper[sortedkeys[1]][0], mapper[sortedkeys[1]][1]))
+    #print("Last index is 0x{0:02X}".format(len(mapper)-1))
 
 def convert_palette(palette, filebase, p):
     counter = 0
@@ -320,15 +378,18 @@ def main():
     parser.add_argument("--width", type=int, default=1, help="Meta tile width (default: 1)")
     parser.add_argument("--height", type=int, default=1, help="Meta tile height (default: 1)")
     parser.add_argument("--output", "-o", default="", help="Base name for output files (default: derived from image name)")
-    parser.add_argument("--datarom", "-r", default="", help="Address within the ROM, data should be placed at")
+    parser.add_argument("--datarom", "-d", default="", help="Address within the ROM, data should be placed at")
     parser.add_argument("--maprom", "-m", default="", help="Address within the ROM, map should be placed at")
     parser.add_argument("--palrom", "-p", default="", help="Address within the ROM, palette should be placed at")
     parser.add_argument("--limit", type=int, default=255, help="Maximum of tiles to put into data (default: 255)")
     parser.add_argument("--compress-rle", "-c", default="no", help="Additionally compress data with improved RLE algorithm (default: no)")
-    parser.add_argument("--compress-map-rle", default="no", help="Additionally compress map with improved RLE algorithm (default: no)")
-    parser.add_argument("--color-line-compression", default="yes", help="Encode rows with just one color in one byte (default: yes)")
-    parser.add_argument("--increment-compression", default="yes", help="Encode incrementing byte sequence (default: yes)")
+    parser.add_argument("--compress-map-rle", "-r", default="no", help="Additionally compress map with improved RLE algorithm (default: no)")
+    parser.add_argument("--color-line-compression", "-l", default="yes", help="Encode rows with just one color in one byte (default: yes)")
+    parser.add_argument("--increment-compression", "-i", default="yes", help="Encode incrementing byte sequence (default: yes)")
     parser.add_argument("--size", "-s", default="no", help="Always print size for non compressed images (default: no)")
+    #parser.add_argument("--extreme-compression", "-x", default="no", help="Try all permutations of the image (default: no)")
+    #parser.add_argument("--permutation-seed", default="0", help="Specify permutation of the image (Find a good one with -x)")
+    #parser.add_argument("--palmap", default="no", help="Concatenate the palette to the mapping array (helpful for RLE de/compression)")
     global args
 
     args = parser.parse_args()
@@ -355,7 +416,7 @@ def main():
             outbase = args.output[:-6]
         if args.output[-6:] == '_pal.c':
             outbase = args.output[:-6]
-    
+
     d = open(outbase + '_data.c', 'w')
     d.write("// Generated by png2gb.py\n")
     if args.compress_rle != "no":
@@ -392,6 +453,11 @@ def main():
         dataaccu += datasize
         mapaccu += maprealsize
         convert_palette(original[3]['palette'], outbase, p)
+    # maybe only do this with --compress-map-rle enabled
+    # but data compression could still improve
+    # only do this when mapping compression is active, so not for --uncompressed
+    # compress both versions and take the smaller one
+    mapping_optimizer(databuffer, mapbuffer)
     # compress everything as a whole
     if args.compress_rle != "no" or args.size != "no":
         print("Before compression: 0x{0:02X} bytes".format(dataaccu))
@@ -435,10 +501,10 @@ def main():
     p.close()
 
     if(mapsize > 256):
-        print("Warning: Too many mapped tiles in tileset: {0} (UINT8/unsigned char maximum is 255)".format(mapsize))
+        print("{1}: warning: Too many mapped tiles in tileset: {0} (UINT8/unsigned char maximum is 255)".format(mapsize, args.image[0]))
 
     if(mapcounter >= args.limit):
-        print("Warning: Too many unique tiles in tileset: {0}".format(mapcounter+1))
+        print("{1}: warning: Too many unique tiles in tileset: {0}".format(mapcounter+1, args.image[0]))
 
 
 main()
